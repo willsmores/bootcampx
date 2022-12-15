5dd398341acadb8c0fccfd1e7026b0fa71433556
Steve Willsmore <scwillsmore@gmail.com>
2022-12-14 20:55:26 -0500

    Add pg dependency. Add teacher app page.

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,29 @@
+const [, , cohortName] = process.argv;
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'labber',
+  password: 'labber',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+
+/* Pool Queries */
+
+pool.query(`
+      SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+      FROM teachers
+      JOIN assistance_requests ON teachers.id = teacher_id
+      JOIN students ON students.id = student_id
+      JOIN cohorts ON cohorts.id = cohort_id
+      WHERE cohorts.name = '${cohortName || 'JUL02'}'
+      ORDER BY teacher;
+    `)
+  .then(res => {
+    res.rows.forEach(row => {
+      console.log(`${row.cohort}: ${row.teacher}`);
+    })
+  })
+  .catch(err => console.error('query error', err.stack));
